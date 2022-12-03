23632e59c6d8f0117d48e23cefbc863fc0dce5f8
pablojwittich <pablojwittich1998@gmail.com>
2022-12-03 14:42:13 -0300

    curso_Java

---
+++ cursos/Java/java_ats.java
@@ -0,0 +1,44 @@
+/*
+JAVA
+1. Historia
+- Creado por Sun Microsystems Inc.
+- Fue presentedao en la segunda mitad de 1995
+- Fue creado por un grupo de trabajo liderado por james Gosling
+- El nombre "Java" surgió de una de la reuniones creativas celebradas por el equipo de desarrollo de lenguaje.
+
+2. Que es Java?
+Java es un lenguaje de desarrollo de propósito general, y como tal es válido para realizar todo tipo de aplicaciones profesionnales.
+
+3. Qué lo hace distitno a los demás lenguajes?
+- Los programas breados por java son independientes de la arquitectura; esto quiere decir a que son portables.
+- Permite escribir Applets. Los Apples son pequeños programas que se insertan en una pagina web.
+- Se pueden escribir aplicaciones para intraredes.
+- Aplicaciones cliente/servidor.
+- Aplicaciones distribuidas en redes locales y en internet.
+- Es facil de aprender y esta bien estructurado.
+
+4. Qué se puede programar con Java?
+- Aplicaciones independientes
+- Applets
+
+5. Caracteristicas del lenguaje
+- Es instrinsecamente orientado a objetos.
+- Funciona perfectamente en red.
+- Tiene gran funcionalidad gracias a sus librerias (clases).
+- No tiene punteros manejables por el programador, aunque los maneja la maquina interna y transparentemente.
+- Genera aplicaciones con pocos errores posibles.
+- Incorpora Multi-Theanding (Multi-Hilos). Sirve para que las operaciones concurrentes se ejecuten ordenadamente.
+
+ 
+*/  
+// 6. Mi primer programa en Java
+public class java_ats { // A tener el cuenta que el nombre tiene que ser el mismo del archivo!
+    /**
+     * @param argas the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Hola Mundo!"); // Tipiando "sout" nos rellenar esta porción del codigo
+    }
+    
+        
+    } 
