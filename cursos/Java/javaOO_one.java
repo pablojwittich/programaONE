75ba992cd7c93ed0fa7213031d90d5f002e3c268
pablojwittich <pablojwittich1998@gmail.com>
2023-01-27 09:19:18 -0300

    Curso de Java del Programa ONE

--- cursos/Java/javaOO_one.java
+++ cursos/Java/javaOO_one.java
@@ -1,4 +1,16 @@
 /* 
- 
+La formación Java Orientado a Objetos de Alura es una guia de aprendizaje completo para que inicies una carrera en programación Java. Aprenderás desde los fundamentos del lenguaje con buenas prácticas hasta el conocimiento esencial para un buen desarrollo orientado a objetos.
+
+1.SOBRE JAVA
+    - Es uno de los lenguajes de programación mas utilizados en el mundo según el ranking TIOBE.
+    - Lenguaje orientado a objetos, multiplataforma.
+    - Uno de los mas completos y con diversidad de bibliotecas.
+
+2. Entender el lenguaje Java y orientado a objetos
+El primer paso es entender el lenguaje Java, que es la base de todo lo que haremos en esta formación. Si quieres aprender a desarrollar un sitio web, el servidor o el back-end de una aplicación, o hasta una aplicación mobile que utiliza Java, este contenido te ayudará en tu camino. 
+Para ello es esencial desarrollar un conocimiento sólido en orientación a objetos y buenas prácticas de código. De este modo, aquí aprenderás desde recursos básicos del lenguaje, como variables, controles de flujo y ciclos de repetición, hasta los principales conceptos del paradigma de orientación a objetos, como **clases, herencia e interfaces y como todo eso te ayudará en tu día a día en la programación.
+
+3. Java JRE y JDK: Compile y ejecute su primer programa
+
 
  */
